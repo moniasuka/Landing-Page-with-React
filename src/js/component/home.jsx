4d8e487768e4b0fc012ac44b3f1da0dff5954dd8
moniasuka <moniasuka@hotmail.com>
2022-09-27 02:47:32 +0000

    pushing navbar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,17 +2,15 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-import navbar from 
+import {Navbar} from "../component/navbar.jsx";
+
 
 //create your first component
 const Home = () => {
 	return (
 		<>
-
 		<Navbar />
-		<Carrousel />
-		<Cards />
-		<Footer />
+		
 
 
 		</>
--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,13 +1,33 @@
 import React from "react";
+export const Navbar = () => {
 
-
-export const navbar = () => {
 return(
 <>
-
-
-
-
+<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Start Bootstrap</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse float-end" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">About</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Services</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Contact</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
 </>
 )
 }
